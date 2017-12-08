854946ca300ebd24f7608fdc5c25730ad19686c1
tcz <git@tcz.hu>
2017-12-08 16:55:00 +0100

    Adding request and response time and real IP

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -2,11 +2,15 @@ http {
     include       mime.types;
     default_type  application/octet-stream;
 
-    #log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-    #                  '$status $body_bytes_sent "$http_referer" '
-    #                  '"$http_user_agent" "$http_x_forwarded_for"';
-
     #access_log  logs/access.log  main;
+
+    set_real_ip_from 10.0.0.0/8;
+    real_ip_header X-Forwarded-For;
+
+    log_format timed_combined '$remote_addr - $remote_user [$time_local] "$request" '
+        '$status $body_bytes_sent "$http_referer" '
+        '"$http_user_agent" '
+        'reqtime=$request_time resptime=$upstream_response_time pipe=$pipe';
 
     sendfile        on;
     #tcp_nopush     on;
@@ -55,7 +59,7 @@ http {
         root "<?=getenv('DOCUMENT_ROOT')?:getenv('HEROKU_APP_DIR')?:getcwd()?>";
         
         error_log stderr;
-        access_log /tmp/heroku.nginx_access.<?=getenv('PORT')?:'8080'?>.log;
+        access_log /tmp/heroku.nginx_access.<?=getenv('PORT')?:'8080'?>.log timed_combined;
         
         include "<?=getenv('HEROKU_PHP_NGINX_CONFIG_INCLUDE')?>";
         
